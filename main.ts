bd4fd00ada0c6549afb8c7e1fc66c64ad8c009ea
Ahmed-Ali-0 <71400128+Ahmed-Ali-0@users.noreply.github.com>
2020-10-01 12:13:21 -0400

    Add comment and traffic light  program

--- main.ts
+++ main.ts
@@ -1,3 +1,28 @@
+// Created by: Ahmed Ali
+// Created Date:Oct 1, Thursday
+// 
+// This program used to  traffic light 
+let strip = neopixel.create(DigitalPin.P16, 3, NeoPixelMode.RGB)
+strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+basic.showIcon(IconNames.Yes)
+strip.show()
+basic.pause(2000)
+strip.clear()
+strip = neopixel.create(DigitalPin.P16, 2, NeoPixelMode.RGB)
+strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+basic.showIcon(IconNames.Happy)
+strip.show()
+basic.pause(500)
+strip.clear()
+strip = neopixel.create(DigitalPin.P16, 2, NeoPixelMode.RGB)
+strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+basic.showIcon(IconNames.Heart)
+strip.show()
+basic.pause(5000)
+strip.clear()
 basic.forever(function () {
-	
+    strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+    strip.clear()
+    strip.show()
+    basic.clearScreen()
 })
